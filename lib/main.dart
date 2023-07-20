48f74832382ff8b2960768512bd2cb36c2efc614
AbdelrhmanAbuelnile <abdohani362@gmail.com>
2023-07-20 04:04:25 +0300

    hi

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:location/location_page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Location Demo',
+      debugShowCheckedModeBanner: false,
+      home: LocationPage(),
+    );
+  }
+}
